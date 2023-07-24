c0696a82e1f33066b0a51a669460dc0894cf24c4
PrebenS <prebens.kodehode@gmail.com>
2023-07-24 02:13:17 +0200

    changed js

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const weightInput = document.querySelector("#input-el");
 const calcBtn = document.querySelector("button");
 const weightOutput = document.querySelectorAll("h3[data-gravity]");
 
+weightOutput.forEach((h3) => {
+  h3.textContent = `0.00`;
+});
+
 calcBtn.addEventListener("click", function calcWeight() {
   weightOutput.forEach((h3) => {
     const gravityMultiplier = h3.getAttribute("data-gravity");
