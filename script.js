087e0812809fe3f0dacddfa8f10bc15fd027ded9
PrebenS <prebens.kodehode@gmail.com>
2023-07-24 02:07:26 +0200

    changed js

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-const weightInput = parseFloat(document.querySelector("#input-el").value);
+const weightInput = document.querySelector("#input-el");
 const calcBtn = document.querySelector("button");
 const weightOutput = document.querySelectorAll("h3[data-gravity]");
 
 calcBtn.addEventListener("click", function calcWeight() {
   weightOutput.forEach((h3) => {
-    const gravityMultiplier = parseFloat(h3.getAttribute("data-gravity"));
-    console.log(gravityMultiplier);
-    const planetWeight = weightInput * gravityMultiplier;
-    console.log(planetWeight);
+    const gravityMultiplier = h3.getAttribute("data-gravity");
+
+    const planetWeight = weightInput.value * gravityMultiplier;
+
     h3.textContent = `${planetWeight.toFixed(2)}`;
   });
 });
