269117a3c4969a4fa688b16447a8fb682ac1d0a3
PrebenS <prebens.kodehode@gmail.com>
2023-07-26 09:45:34 +0200

    creates elements in js

--- script.js
+++ script.js
@@ -1,10 +1,43 @@
 const weightInput = document.querySelector("#input-el");
 const calcBtn = document.querySelector("button");
-const weightOutput = document.querySelectorAll("h3[data-gravity]");
+const mainWrapper = document.querySelector("main");
 
-weightOutput.forEach((h3) => {
-  h3.textContent = `0.00`;
-});
+const planets = [
+  { name: "the Sun", gravity: 27.01, imgName: "sun" },
+  { name: "Mercury", gravity: 0.38, imgName: "mercury" },
+  { name: "Venus", gravity: 0.91, imgName: "venus" },
+  { name: "Earth", gravity: 1, imgName: "earth" },
+  { name: "the Moon", gravity: 0.166, imgName: "moon" },
+  { name: "Mars", gravity: 0.38, imgName: "mars" },
+  { name: "Jupiter", gravity: 2.34, imgName: "jupiter" },
+  { name: "Ganymede", gravity: 0.146, imgName: "ganymede" },
+  { name: "Saturn", gravity: 1.06, imgName: "saturn" },
+  { name: "Uranus", gravity: 0.92, imgName: "uranus" },
+  { name: "Neptune", gravity: 1.19, imgName: "neptune" },
+  { name: "Pluto", gravity: 0.06, imgName: "pluto" },
+];
+
+for (let planet of planets) {
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const planetImg = document.createElement("img");
+  planetImg.src = `./Images/${planet.imgName}.webp`;
+  planetImg.alt = `close up photo of ${planet.name}`;
+
+  const planetTitle = document.createElement("h3");
+  planetTitle.textContent = planet.name;
+
+  const planetText = document.createElement("p");
+  planetText.textContent = `Your weight on ${planet.name} would be`;
+
+  const planetWeight = document.createElement("h3");
+  planetWeight.dataset.gravity = `${planet.gravity}`;
+  planetWeight.textContent = 0.0;
+
+  card.append(planetImg, planetTitle, planetText, planetWeight);
+  mainWrapper.append(card);
+}
 
 // prevents user to input number outside the set min/max
 weightInput.oninput = function () {
@@ -25,6 +58,8 @@ weightInput.oninput = function () {
 
 // calculates the weights
 function calcWeight() {
+  const weightOutput = document.querySelectorAll("h3[data-gravity]");
+
   weightOutput.forEach((h3) => {
     const gravityMultiplier = h3.getAttribute("data-gravity");
 
